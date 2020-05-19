42d1046e1e67282b4a93f5426c2d4c8384397b4b
jackokramer <jackokramer@gmail.com>
2020-05-19 14:21:17 -0700

    satan

--- binarytrees/binarysearchtree.js
+++ binarytrees/binarysearchtree.js
@@ -33,9 +33,18 @@ class BinarySearchTree{
         }
         return;
     }
+    //size
+    size(position = this.root){
+        if(position == null){
+            return 0;
+        }
+        return 1 + this.size(position.left) + this.size(position.right);
+    }
 }
 
 let tree = new BinarySearchTree();
 tree.addNode(78);
 tree.addNode(29);
 tree.addNode(20);
+tree.addNode(17);
+console.log(tree.size())
