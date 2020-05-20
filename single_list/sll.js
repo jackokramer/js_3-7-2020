5874eb5457274dce0fd86ad3b734bd8fc9660c0c
jackokramer <jackokramer@gmail.com>
2020-05-20 10:44:56 -0700

    slave

--- single_list/sll.js
+++ single_list/sll.js
@@ -6,5 +6,20 @@ class Node{
 }
 
 firstNode = new Node(6);
+secondNode = new Node(17);
 
 console.log(firstNode);
+
+firstNode.next = secondNode;
+
+console.log(firstNode.next);
+
+class SLL{
+    constructor(node){
+        this.head = node;
+    }
+}
+
+firstSll= new SLL(firstNode);
+
+console.log(firstSll);
