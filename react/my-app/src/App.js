4f0b38e4ea96c58ddef5cd982d8073a7cc6f8558
jackokramer <jackokramer@gmail.com>
2020-05-24 19:57:48 -0700

    ready

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -24,10 +24,11 @@ import './App.css';
 } */}
 
 const  App = () =>{
+  const message = 'This is my first variable in JSX'
   return(
   <div>
-    <h1>Full Name</h1>
-    <h2>My first React App</h2>
+    <h1 className="text-center">Full Name</h1>
+    <h2 className="text-center p-2 m-3">My first React App</h2>
     </div>
   )
 }
