21e0b2e19478446ddc59c816b0ea2da004698147
jackokramer <jackokramer@gmail.com>
2020-05-27 21:17:40 -0700

    reqa

--- objects/playingwiv.js
+++ objects/playingwiv.js
@@ -2,4 +2,10 @@
 
 let users = [{name: 'Peter', age:51}, {name: 'Molly', age:27}, {name:'Shannon', age:32}, {name:'Sasha', age:25}]
 
-console.log(users[0].age);
+console.log(users[2].age);
+
+console.log(users[3].name);
+
+for(let x = 0; users.length > x; x++){
+    console.log(users[x].name, '-', users[x].age);
+}
