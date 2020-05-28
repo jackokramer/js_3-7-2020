02a1b78cf73254c6d591aedeec6b738e84a841a2
jackokramer <jackokramer@gmail.com>
2020-05-28 09:05:00 -0700

    erq

---
+++ react/test_project/app.js
@@ -0,0 +1,8 @@
+const myButton = React.createElement('button', null, 'Click Me');
+console.log(JSON.stringify(myButton));
+
+React.createElement('h1', null, "hellow");
+
+React.createElement('p', null, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus accusamus sit repudiandae aliquid ab earum a, minus voluptate accusantium nisi ratione et provident quis laborum eaque voluptas non molestias soluta.")
+
+ReactDOM.render(myButton, document.getElementById('app'))
