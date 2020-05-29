872c76bd4ea64400ddaa01da6fa362771a8c22d2
jackokramer <jackokramer@gmail.com>
2020-05-29 10:14:00 -0700

    sqaured

--- react/com_squares/squares/src/App.js
+++ react/com_squares/squares/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <div>
-        
+      <div className="bluebox">
+        <p className="white">White on Blue</p>
+      </div>
+      <div className="redbox">
+        <p className="blue">Blue on red</p>
+      </div>
+      <div className="pinkbox">
+        <p className="green">green on pink</p>
       </div>
         <a
           className="App-link"
--- react/com_squares/squares/src/index.js
+++ react/com_squares/squares/src/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import learnReact from './learnReact';
 import * as serviceWorker from './serviceWorker';
-
-const Bigbox = React.createElement("div", {style:{color:'blue'}, className="bluebox"}, "White on Blue");
-  React.createElement("div", {style:{color:'red'}, className="redbox"},"Blue on Red");
-  React.createElement("div", {style:{color:'pink'}, className="pinkbox"}, "Green on pink");
-);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <learnReact />
   </React.StrictMode>,
   document.getElementById('root')
 );
