2533618b53a09040f9a6b05ba8b2c7138b1b58f8
jackokramer <jackokramer@gmail.com>
2020-05-29 15:36:21 -0700

    ready

---
+++ react/cards/my-app/src/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./index.css";
+
+function Card (){
+    return (
+    <div className="card">
+        <img src="./public/images/iu.gif" alt="gorilla_boi" class="gorija mx-auto"/>
+        <h1 className='tit text-center'>Front End Dev.</h1>
+        <p className="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores deleniti laboriosam commodi possimus soluta eveniet illum sit magnam ullam ipsa suscipit, iste laudantium totam similique laborum sequi voluptate. Eius, ratione. Lorem ipsum dolor sit amet consectetur adipisicing elit. Est natus totam delectus sed temporibus minus nesciunt debitis nam saepe quisquam. Excepturi iste deleniti dignissimos temporibus? Sed est similique enim harum.</p>
+    </div>
+        );
+}
+
+export default Card;
