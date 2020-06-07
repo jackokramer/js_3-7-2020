2a5af274dff51ad4961d93f0fef639758dd27acb
jackokramer <jackokramer@gmail.com>
2020-06-06 23:31:21 -0700

    awq

--- js_chall/day_4/main.js
+++ js_chall/day_4/main.js
@@ -1,7 +1,19 @@
 function sumAllPrimes(num){
     //solution
+    let primeNum = 0
+    for(let x = 2; num>x; x++){
+        for(let p =2; p<=x; p++){
+            if(x===p){
+                primeNum +=0;
+            }
+            if(x%p === 0){
+                break
+            }
+        }
+    }
 }
 
+console.log(sumAllPrimes(29));
 
 /**
  * Test Suite
