f66dbf4f8236cf36500ba6378a9d91533fd2bd40
jackokramer <jackokramer@gmail.com>
2020-06-07 19:26:21 -0700

    sunday

--- js_chall/main.js
+++ js_chall/main.js
@@ -10,4 +10,30 @@ console.log(evendigitsOnly(12));
  * Test Suite
 */
 
+describe('evendigitsOnly()', ()=>{
+    it('returns true only when digits are even', () =>{
+        //arrange
+        const nums = 248622;
+        //act
+        const result = evendigitsOnly(nums);
+        // log
+        console.log('results 1: ', result);
 
+        //asser 
+        expect(result).toBe(true);
+    })
+})
+
+it('returns flase when any digits are odd', ()=>{
+    //arrange
+    const nums = 642386;
+
+    //act
+    const result = evendigitsOnly(nums);
+
+    // log
+    console.log('result 2: ', result);
+
+    //assert
+    expect(result).toBe(false);
+})
