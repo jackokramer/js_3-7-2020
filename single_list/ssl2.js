a8bd63ba756547dbf9461cc19cb982c71001fa5c
jackokramer <jackokramer@gmail.com>
2020-06-23 16:27:10 -0700

    raww

---
+++ single_list/ssl2.js
@@ -0,0 +1,46 @@
+// implement a linked list nOde class
+class node{
+    constructor(val, next = null){
+        this.val = val;
+        this.next = next;
+    }
+}
+
+/*let n1 = new node(4);
+let n2 = new node(18);
+let n3 = new node(30);
+console.log(n1);
+n1.next = n2;
+console.log(n2);
+console.log(n3);
+*/
+// implement the list class
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    add(val){
+        const newNode = new node(val);
+        if(this.head == null){
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next
+            }
+            current.next = newNode;
+            console.log(`added ${val}`)
+        }
+        this.size++;
+    }
+}
+
+let list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+
+
+// add node/ add node to the end
