61fa5c9a8b0593e295ce4f5bd40b66a3abe20324
jackokramer <jackokramer@gmail.com>
2020-06-23 15:58:16 -0700

    reak

---
+++ single_list/bankaccount.js
@@ -0,0 +1,11 @@
+/*class bankAccount:
+def __init__(self, int_rate=0, balance=0)
+    self.int_rate= int_rate
+    self.balance = balance
+
+def deposit(self, amount):
+    self.balance +=amount
+    return self
+
+ba1 = bankAccount(balance=100)
+ba2 = bankAccount(balance=200)*/
