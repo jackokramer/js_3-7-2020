2dc100f088b38e0f1989bf006ab67fac89bb4919
jackokramer <jackokramer@gmail.com>
2020-06-22 20:51:19 -0700

    reaq

---
+++ single_list/pract.js
@@ -0,0 +1,8 @@
+//push front given an array and an additional value, insert the value at the beginning of an array. Do this withoyt a built in system.
+
+function pushFrony(arr, val){
+    let newArr = [];
+    for(let x = 0; arr.length>x; x++){
+        //push all each value in array over once
+    }
+}
