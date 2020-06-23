70bad9874268846342183589990b875163246100
jackokramer <jackokramer@gmail.com>
2020-06-23 16:04:14 -0700

    area

--- single_list/bankaccount.js
+++ single_list/bankaccount.js
@@ -9,3 +9,22 @@ def deposit(self, amount):
 
 ba1 = bankAccount(balance=100)
 ba2 = bankAccount(balance=200)*/
+//ba1.deposit(100)
+
+class bankAccount{
+    constructor(intRate = 0, balance=0){
+        this.intRate= intRate;
+        this.balance= balance;
+    }
+
+    deposit(amount){
+        this.balance += amount;
+        console.log(`${amount} has been added to your account!`)
+        //return this;
+    }
+}
+
+let bank1 = new bankAccount(0.08, 200);
+console.log(bank1.balance);
+bank1.deposit(500);
+console.log(bank1.balance);
