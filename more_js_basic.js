e8964ab9ab6b48f2249e610dac9455990a3af9f9
jackokramer <jackokramer@gmail.com>
2020-06-22 18:36:58 -0700

    reaw

--- more_js_basic.js
+++ more_js_basic.js
@@ -1,14 +1,3 @@
-//Swap Toward the Center - Given an array, swap the first and last values, third and third-to-last values, etc.  Example: swapTowardCenter([true,42,"Ada",2,"pizza"]) turns the array into ["pizza", 42, "Ada", 2, true].  swapTowardCenter([1,2,3,4,5,6]) turns the array into [6,2,4,3,5,1].  No need to return the array this time.
-
-function swapTowardCenter(arr){
-    let poop = [];
-    for(let z = 0; arr.length>z; z++){
-        arr[x] = arr.length -1;
-        arr[arr.length-1] = arr[x];
-        arr.length - x-x;
-    }
-}
-
 //doublevision
 
 function doubleVision(arr){
@@ -43,6 +32,29 @@ function doubs(arr){
     for(let x = 0; arr.length>x; x++){
             news.push(arr[x]*2);
         }
-    }
+
     return news
+    }
+
+//Count Positives - Given an array of numbers, create a function to replace the last value with the number of positive values found in the array.  Example, countPositives([-1,1,1,1]) changes the original array to [-1,1,1,3] and returns it.
+
+function countPostives(arr){
+    let post = 0
+    for(let x = 0; arr.length>x; x++){
+        if(arr[x]>0){
+            post++
+        }
+    }
+    arr[arr.length -1] = post;
+    return arr;
+}
+
+// Swap Toward the Center - Given an array, swap the first and last values, third and third-to-last values, etc.  Example: swapTowardCenter([true,42,"Ada",2,"pizza"]) turns the array into ["pizza", 42, "Ada", 2, true].  swapTowardCenter([1,2,3,4,5,6]) turns the array into [6,2,4,3,5,1].  No need to return the array this time.
+
+function swapsTowards(arr){
+    for(let x = 0; arr.length/2>x; x+2){
+        let temp = arr[x];
+        arr[x] = arr[arr.length-1-x];
+        arr[arr.length-1-x] = temp;
+    }
 }
