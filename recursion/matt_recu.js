50337fc8da7278c6c92ff3b5209bca79f439facd
jackokramer <jackokramer@gmail.com>
2020-06-23 17:13:46 -0700

    reaq

---
+++ recursion/matt_recu.js
@@ -0,0 +1,17 @@
+/*let num = 3;
+let x = 0
+
+while(x<num){
+    console.log('projects and algos 2020');
+    x++;
+}
+*/
+//recursion is no different basically calls itself in a function
+
+
+function boastPA(){
+    console.log("projects and algos are a pain");
+    boastPA();
+}
+
+boastPA(); // same as an undefined while loop
