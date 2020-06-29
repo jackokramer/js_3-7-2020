d8268c0972a9c960813eaf77e8c9d0e7fc61d72e
jackokramer <jackokramer@gmail.com>
2020-06-29 09:21:12 -0700

    tread

--- js_chall2/day_4/main.js
+++ js_chall2/day_4/main.js
@@ -1,5 +1,14 @@
 function arrayReplace(array, elemToReplace, substitutionElem){
-    // write code here
+    // iterate through the list
+    for(let x =0; array.length>x; x++){
+        //check for a matching number
+        if(array[x] === elemToReplace){
+            //update the match
+            array[x] = substitutionElem;
+        }
+    }
+    return array;
+    //return value
 }
 
 /*
