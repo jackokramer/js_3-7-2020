5ed2540b2c220eaed7288f4c3524e9d86eaf8cea
jackokramer <jackokramer@gmail.com>
2020-06-29 10:21:10 -0700

    tree

--- binarytrees/june_tree.js
+++ binarytrees/june_tree.js
@@ -1,4 +1,4 @@
-class treeNode{
+/*class treeNode{
     constructor (val){
         this.val = val;
         this.left = null;
@@ -29,3 +29,74 @@ function inOrderPrint(root){
 }
 
 inOrderPrint(one);
+*/
+
+
+/* Binary Search Trees
+15, 6, 17, 3, 5, 18
+
+3,5,6,15,17, 18
+*/
+
+class treeNode{
+    constructor (val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST{
+    constructor(){
+        this.root = null;
+    }
+    add(val, root = this.root){
+        // if the tree is empty, new node is the root
+        if (this.root == null){
+            this.root = new treeNode(val);
+            return;
+        }
+        if(val < root.val){
+            if(root.left === null){
+                root.left = new treeNode(val);
+            } else {
+                this.add(val, root.left)
+            }
+        } else{
+            if(root.right === null){
+                root.right = new treeNode(val);
+            } else{
+                this.add(val, root.right);
+            }
+        }
+    }
+     inOrderPrint(root = this.root){
+        if(root === null)return;
+        inOrderPrint(root.left);
+        inOrderPrint(root.right);
+        console.log(root.val);
+    }
+    search(val, root=this.root){
+        if(this.root === null) return false;
+
+        if(val<root.val){
+            return this.search(val, root.left);
+        } else if(val>root.val){
+            return this.search(val, root.right);
+        } else {
+            return true;
+        }
+    }
+}
+
+let tree = new BST();
+tree.add(15);
+tree.add(6);
+tree.add(17);
+tree.add(3);
+tree.add(5);
+tree.add(18);
+
+console.log(tree.root);
+
+console.log(tree.search(4))
