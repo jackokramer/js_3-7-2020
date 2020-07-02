0105c341a56df3815490450a2246f029f2ead760
jackokramer <jackokramer@gmail.com>
2020-07-02 14:34:52 -0700

    sea

--- js_chall2/day_7/main.js
+++ js_chall2/day_7/main.js
@@ -5,7 +5,13 @@ function factorialNum(num){
     } else{
         return num * factorialNum(num-1);
     }
-
+    // Dylan's solution
+/*    let sum = 1;
+    for(let x = 1; x<=num; x++){
+        sum*=x;
+    }
+    return sum;
+    */
 }
 
 /*
