4a6a4da0a53668b06e27ead3912c5ee3fd048d4a
jackokramer <jackokramer@gmail.com>
2020-07-07 09:41:44 -0700

    sllep

---
+++ js_chall2/all_chall/day5.js
@@ -0,0 +1,35 @@
+function caseInsensitvePalin(str){
+    // write code here
+    //make str caseless
+    const caslessStr = str.toLowerCase();
+    const reverseStr = caslessStr.split(''.reverse().join(''));
+    return caslessStr === reverseStr;
+}
+
+describe('caseInsensitvePalin()', () => {
+    it('returns true for a case insensitve palindrome', ()=>{
+        //arrange
+        const str= 'AaBaa';
+
+        //act
+        const result = caseInsensitvePalin(str);
+
+        //log
+        console.log('result 1:', result);
+
+        //assert
+        expect(result).toBe(true);
+    })
+    it('returns false when not a case insensitve palindrome', ()=>{
+        //arrange
+        const str = 'abac';
+        //act
+        const result = caseInsensitvePalin(str);
+
+        //log
+        console.log('result 2:', result);
+
+        //assert
+        expect(result).toBe(false);
+    })
+})
