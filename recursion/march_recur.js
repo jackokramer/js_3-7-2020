9df592e43d292b09813491bfeed5f163635203ae
jackokramer <jackokramer@gmail.com>
2020-07-09 12:48:33 -0700

    thur

---
+++ recursion/march_recur.js
@@ -0,0 +1,15 @@
+//Function review
+
+function aRecursion(times){
+    //base case
+    if(times>0){
+        aRecursion(times = times-1);
+    }
+}
+aRecursion(10);
+
+let num = 0;
+while(num<10){
+    console.log(num);
+    //num++;
+}
