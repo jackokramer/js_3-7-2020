17b8e1b45fa8c2ca1ea4f4a0d3eecedd4f14f55b
jackokramer <jackokramer@gmail.com>
2020-07-08 21:03:42 -0700

    ew

---
+++ single_list/ssl_feb.js
@@ -0,0 +1,11 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+firstNode = new Node(6);
+secondNode = new Node(10);
+
+console.log(firstNode);
