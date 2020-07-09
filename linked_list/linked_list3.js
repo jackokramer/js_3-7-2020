cea29236ea107fcc6d8fd1e4309529669e8cfae0
jackokramer <jackokramer@gmail.com>
2020-07-08 21:42:34 -0700

    reweq

--- linked_list/linked_list3.js
+++ linked_list/linked_list3.js
@@ -5,12 +5,23 @@ class listNode{
     }
 }
 
+firstNode = new listNode(7);
+
 class singleList{
     constructor(){
         this.head = null;
     }
     //SList: Back
     //Create a function that accepts a ListNode pointer and returns the last value in the list.
+    addBack(value){
+        let runner = this.head;
+        while(runner.next){
+            console.log(`this is the current value${runner.value}, this is the next node ${runner.next}`)
+            runner = runner.next;
+        }
+        runner.next = new listNode(value);
+        return this
+    }
 
     //SList: Remove Back
     //Create a standalone function that removes the last ListNode in the list and returns the new list.
@@ -18,3 +29,7 @@ class singleList{
     //SList: Add Back
     //Create a function that creates a ListNode with given value and inserts it at end of a linked list.
 }
+
+firstEx =  new singleList(firstNode);
+
+firstEx.addBack(7)
--- single_list/ssl_feb.js
+++ single_list/ssl_feb.js
@@ -6,6 +6,54 @@ class Node{
 }
 
 firstNode = new Node(6);
-secondNode = new Node(10);
+//secondNode = new Node(10);
 
+//console.log(firstNode);
+
+/*firstNode.next =  secondNode;
+thirdNode = new Node(9);
+fourthNode = new Node(17);
+secondNode.next = thirdNode;
+thirdNode.next =fourthNode;
+console.log(firstNode.next);
 console.log(firstNode);
+*/
+class SLL{
+    constructor(node){
+        this.head = node;
+    }
+    view(){
+        let runner = this.head;
+        let count = 0;
+        while(runner){
+            console.log(`you are on Node: ${count}, it's value is ${runner.value}`)
+            runner = runner.next;
+            count +=1; // makes it count
+        }
+        return this
+        //this.head // piunter to the first node
+    }
+    addFront(value){
+        let temp = this.head;
+        this.head = new Node(value);
+        this.head.next = temp;
+        console.log("A new node has been added to the front")
+        return this;
+    }
+    addBack(value){
+        //find end of the list
+        // add node
+        let runner = this.head;
+        while(runner.next){
+            console.log(`this is the current: value ${runner.value}, this is the next node: ${runner.next}`)
+            runner = runner.next;
+        }
+        runner.next = new Node(value);
+        return this;
+    }
+}
+
+firstSLL = new SLL(firstNode);
+
+//console.log(firstSLL);
+firstSLL.view().addFront(10).addFront(12).addFront(14).addFront(22).view().addBack('sup').view();
