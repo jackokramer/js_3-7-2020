304ca4b901a61ea9582001fc148fe54a9e4c9fac
jackokramer <jackokramer@gmail.com>
2020-07-13 20:54:39 -0700

    seagull

--- linked_list/linked_list2.js
+++ linked_list/linked_list2.js
@@ -14,7 +14,14 @@ class singleList{
 
 //List: Contains
 //Sam thinks Tad might be somewhere in a very long line waiting to attend the Superman movie. Given a ListNode pointer and a val, return whether val is found in any node in the list.
-
+    contains(value){
+        let val = new listNode;
+        if(this.head = value || this.next){
+            return val;
+        } else {
+            return null;
+        }
+    }
 
  // SList: Length
 //July 20, 2013: about 5000 people wait in line for a chance to audition for American Idol. Create a function that accepts a pointer to the first list node, and returns number of nodes in that SList.
