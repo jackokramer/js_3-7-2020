a3b4a6838634f738bf4471661405a17469142959
jackokramer <jackokramer@gmail.com>
2020-07-14 10:25:36 -0700

    tues

--- linked_list/linked_list4.js
+++ linked_list/linked_list4.js
@@ -11,10 +11,17 @@ class singleList{
     }
     //SList: Prepend Val
     //Create prependVal(ListNode,val,before) to insert a new ListNode with val immediately before the node containing before (or at end, if no node contains before). Return the new list.
+    prependVal(ListNode, value, before){
 
+    }
     //SList: Append Val
     //Create appendVal(list,val,after) that inserts a new ListNode containing given val immediately after the node containing after (or at end, if after not found). Return the new list.
+    appendVal(list, value, after){
 
+    }
     //SList: Remove Val
     //Create removeVal(ListNode,val). Given a pointer to the head ListNode, remove the node with the given val. Return the new list. What will you do if val is not found?
+    removeVal(listNode, value){
+
+    }
 }
