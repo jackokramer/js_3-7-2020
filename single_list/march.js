e4ccb36c144a01c7efced7cd7a79cbba2ba7cb9c
jackokramer <jackokramer@gmail.com>
2020-07-14 13:21:15 -0700

    tuesday

--- single_list/march.js
+++ single_list/march.js
@@ -1,17 +1,66 @@
 // data structures
 // arrays, dictionaries, classes, list
-
-class Person{
-    // attributes: name, age, location
-    // constructor is the same as def__init__(self) in python
-    constructor(name, age, sex, location){ // when an instance is created the constructor is called  initialization
+/** class Person{
+    constructor(name, age, sex, location){
         this.name = name;
         this.age = age;
         this.sex = sex;
         this.location = location;
+        this.next = null; // hold onto something or hold onto null
+    }
+    makefriend(person){
+        this.next = person;
     }
 }
 
 let jack  = new Person('Jack', 29, "male", "San Francisco"); // invocation of the function call
+let max  = new Person('Max', 30, 'Male', ' San Joe');
+let jordan = new Person('Jordan', 29, 'female', 'Los Angeles');
+let sarah = new Person('Sarah', 31, 'Female', 'San Diego');
 console.log(jack.name); // instance of the person
 console.log(jack.location);
+
+// null is explicitly nothing
+
+jack.next = jordan;
+jordan.next = sarah;
+
+//jack.makefriend(max);
+
+console.log(jack.bestFriend.name);
+*/
+
+class Node{
+    // attributes: name, age, location
+    // constructor is the same as def__init__(self) in python
+    constructor(value){ // when an instance is created the constructor is called  initialization
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class  SLL{
+    constructor(){
+        this.head = null;
+    }
+        addToFront(value){
+            // create a node
+            let newNode = new Node(value);
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        displayValues(){
+            //display each value in the list of nodes
+        }
+}
+
+let myList = new SLL();
+myList.addToFront(17);
+myList.addToFront(5);
+myList.addToFront(42);
+myList.addToFront(90);
+
+console.log(myList)
+
+
