94169dbc9e9ab6e91e25cc8fe66431084e090ad2
jackokramer <jackokramer@gmail.com>
2020-07-16 10:30:32 -0700

    thurs

---
+++ single_list/may.js
@@ -0,0 +1,5 @@
+/* classes have attributes an metjods
+
+// nodes have the value it contains and -> next
+
+sinlgly linked lists only track the first node. used for queues*/
--- single_list/pet.js
+++ single_list/pet.js
@@ -15,7 +15,7 @@ class Node{
 }
 
 firstNode = new Node(6);
-secondNode = new Node(9);
+/*secondNode = new Node(9);
 
 console.log(firstNode.next);
 firstNode.next = secondNode
@@ -26,6 +26,7 @@ fourthNode = new Node(17);
 secondNode.next = thirdNode;
 
 thirdNode.next = fourthNode;
+*/
 
 //console.log(firstNode);
 
@@ -35,12 +36,35 @@ class SLL{
     }
     view(){
         let runner = this.head;
+        let count = 1
         while(runner){
-
+            console.log(`You are on node ${count}, its value is: ${runner.value}`)
+            runner = runner.next;
+            count += 1;
         }
+        return this
+    }
+    addFront(value){
+        let temp = this.head;
+        this.head = new Node(value);
+        this.head.next = temp;
+        console.log(`a new node ${value} has been added to the front of the list`)
+        return this
+    }
+    addBack(value){
+        // find the end of the list... and add null with the new node
+        let runner = this.head;
+        while(runner.next){
+            console.log(`this is the current value: ${runner.value}, this is the next node ${runner.next}`)
+            runner = runner.next;
+        }
+        runner.next = new Node(value);
+        return this
     }
 }
 
 firstSLL = new SLL(firstNode);
 
 console.log(firstSLL);
+
+firstSLL.view().addFront(5).addFront('hi').addFront(11).addFront(23).addBack('Hi').addBack(99).addFront('Front World').view();
