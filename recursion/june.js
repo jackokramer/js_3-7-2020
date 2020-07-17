61c66756a7838f40b8416e8fb013f74e5e7522c4
jackokramer <jackokramer@gmail.com>
2020-07-16 22:27:36 -0700

    thursday

--- recursion/june.js
+++ recursion/june.js
@@ -37,3 +37,51 @@ console.log(factorial(9))
      }
      return fact;
  }
+
+
+/**
+ * Recursive binary search
+ * 
+ * given a sorted array and value, recursively determine whether the value is the array.
+ rBinarySearch = ([1,3,4,5,2], 2) = True
+ rBinarySearch = ([1,2,3,4,7,9], 5) = False
+ */
+ // Return Boolean. True / False
+
+ let sortedArray = [1,3,4,5,22,112,3,43,27];
+
+function rBinarySearch(arr, targ){
+    // base case
+    //if(arr.length === 0) return false;
+
+    let middleIndex = Math.floor(arr.length/2);
+    let leftArr = arr.slice(0, middleIndex);
+    let rightArr = arr.slice(middleIndex+1);
+
+        //recursive case
+    if(targ < arr[middleIndex]){
+        return rBinarySearch(leftArr, targ);
+    } else if (targ > arr[middleIndex]){
+        return rBinarySearch(rightArr, targ)
+    } else {
+        return true;
+    }
+}
+
+console.log(rBinarySearch(sortedArray, 5))
+
+/**
+ * Recursive fibonacci
+ * write rFib(num). Recursively compute and return numth fibonacci value. As earlier  treat the first tow (num=0, num=1).
+ * rfib(2) =1 (0+1)
+ * rfib(3) =1 (1+1)
+ */
+
+function rFib(num){
+    //base case
+    if(num === 1 || num ===2)return 1;
+    //recursive case
+    return rFib(num -1) + rFib(num-2);
+}
+
+console.log(rFib(10))
