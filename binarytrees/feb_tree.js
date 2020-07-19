3406dca64740ea8be512078206da417e2bcef1f0
jackokramer <jackokramer@gmail.com>
2020-07-19 16:37:02 -0700

    bloody sunday

--- binarytrees/feb_tree.js
+++ binarytrees/feb_tree.js
@@ -1,7 +1,65 @@
 //binary Search trees
 
-class binaryNode{
-    constructor(){
+/**
+ * Binary search trees can only have two nodes breaking them off
+ */
 
+class binaryNode{ 
+    constructor(value){
+        this.head = value;
+        this.right = null;
+        this.left = null;
     }
 }
+
+class BST{
+    constructor(){
+        this.root = null; // can be head if necessary
+    }
+    //adding a node - smaller nodes to the left higher nodes than root to the right
+    add(value){
+        let runner = this.root;
+        while(runner){
+            if(value<=runner.value){
+                if(runner.left){
+                    runner = runner.left;
+                } else{
+                    runner.left = new binaryNode(value);
+                    return this
+                }
+            } else{
+                if(runner.right){
+                    runner = runner.right
+                } else{
+                    runner.right = new binaryNode(value)
+                    return this
+                }
+            }
+        }
+        this.root = new binaryNode(value);
+        return this
+    }
+    //finding size
+    size(){
+        if (this.root == null){
+            return 0
+        } function sizeHelp(runner){
+            if(!runner){
+                return 0
+            }
+            return 1 + sizeHelp(runner.left) + sizeHelp(runner.right);
+        }
+        return sizeHelp(this.root)
+    }
+}
+
+//test tree
+tree = new BST();
+
+//adding some nodes
+tree.add(10).add(17).add(9).add(5)
+
+//testing size
+console.log(tree.size);
+
+console.log(tree);
