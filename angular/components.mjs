cb2a1637f57ad41462a5a54e2210d5cd55b97570
jackokramer <jackokramer@gmail.com>
2020-07-23 09:40:41 -0700

    thurs

---
+++ angular/components.mjs
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+@Component({
+    selector: 'app-root',
+    templateURL: './app.component.html',
+    sytleUrls: ['./app.components.css']
+})
+
+export class AppComponent {
+    title ='app works';
+}
+
