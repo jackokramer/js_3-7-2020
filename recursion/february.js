ba07fbf75c791d2bb57238ab93cf5b5204c68c16
jackokramer <jackokramer@gmail.com>
2020-08-02 15:43:14 -0700

    more practice

--- recursion/february.js
+++ recursion/february.js
@@ -13,4 +13,22 @@ function countdown(num){
         console.log(num);
         return countdown(num-1);
     }
+    return 0;
 }
+
+//countdown(78);
+
+//loop array: Write a recursive dfunction to print all values in an inputted array.
+
+//let x = 0;
+function loopArray(arr, x =0){
+    // stay inbound of the array
+    if(x<arr.length){
+        console.log(arr[x]);
+        x++;
+        loopArray(arr, x);
+    }
+    return arr;
+}
+
+loopArray([1,2,34,5,6,77])
