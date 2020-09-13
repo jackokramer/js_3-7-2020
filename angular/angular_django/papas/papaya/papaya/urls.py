5fd930cbb4de36777871af909d93a41dfe16724e
jackokramer <jackokramer@gmail.com>
2020-09-12 18:57:00 -0700

    more angs and djangs

--- angular/angular_django/papas/papaya/papaya/urls.py
+++ angular/angular_django/papas/papaya/papaya/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from papaya_app.views import Papayas, PapayaDetails
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path( 'papaya', Papayas.as_view() ),
+    path('papaya/<int:papaya_id>', PapayaDetails.as_view() )
 ]
+
