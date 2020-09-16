e99120dcf8a5205f617cae6353bcd16c9e5a9428
jackokramer <jackokramer@gmail.com>
2020-09-16 12:23:49 -0700

    dummy done but I need to make some revisions to my code since there are a few bugs https://www.youtube.com/watch?v=eEeUFB1iIDo followed alomg on this tutorial

--- api_guitar/index.js
+++ api_guitar/index.js
@@ -5,8 +5,52 @@ const treble = document.getElementById('treble')
 const visualizer = document.getElementById('visualizer')
 
 const context = new AudioContext()
+const analyzerNode = new AnalyserNode(context, {fftSize: 1024})
+const gainNode = new gainNode(context, {gain: volume.value})
+const bassEQ = new BiquadFilterNode(context, {
+    type: 'lowshelf',
+    frequency: 500,
+    gain: bass.value
+})
+const midEQ = new BiquadFilterNode(context, {
+    type: 'peaking',
+    Q: Math.SQRT1_2,
+    frequency: 1500,
+    gain: mid.value
+})
 
+const trebleEQ = new BiquadFilterNode(context, {
+    type: 'highshelf',
+    frequency: 3000,
+    gain: treble.value
+})
+
+setUpEventListener()
 setUpContext()
+resize()
+drawVisualizer()
+
+function setUpEventListener(){
+    window.addEventListener('resize', resize)
+
+    volume.addEventListener('input', e => {
+        const value = parseFloat(e.target.value)
+        gainNode.gain.setTargetAtTime(value, context.currentTime, .01) // removes popping or clicking sounds
+    })
+    bass.addEventListener('input', e => {
+        const value = parseInt(e.target.value)
+        bassEQ.gain.setTargetAtTime(value, context.currentTime, .01) // removes popping or clicking sounds
+    })
+    mid.addEventListener('input', e => {
+        const value = parseInt(e.target.value)
+        midEQ.gain.setTargetAtTime(value, context.currentTime, .01) // removes popping or clicking sounds
+    })
+    treble.addEventListener('input', e => {
+        const value = parseInt(e.target.value)
+        trebleEQ.gain.setTargetAtTime(value, context.currentTime, .01) // removes popping or clicking sounds
+    })
+}
+
 
 async function setUpContext (){
     const guitar = await getGuitar()
@@ -14,7 +58,13 @@ async function setUpContext (){
         await context.resume()
     }
     const source = context.createMediaStreamSource(guitar)
-    source.connect(context.destination)
+    source
+    .connect(bassEQ)
+    .connect(midEQ)
+    .connect(gainNode)
+    .connect(trebleEQ)
+    .connect(analyzerNode)
+    .connect(context.destination)
 }
 
 function getGuitar(){
@@ -26,4 +76,30 @@ function getGuitar(){
             latency: 0
         }
     })
+}
+
+function drawVisualizer(){
+    requestAnimationFrame(drawVisualizer)
+
+    const bufferLength = analyzerNode.frequencyBinCount // how many frequencies we are measuring
+    const dataArray = new Uint8Array(bufferLength)
+    analyzerNode.getByteFrequencyData(dataArray)
+    const width = visualizer.width;
+    const height = visualizer.height;
+    const barWidth =  width/ bufferLength;
+
+    const canvasContext = visualizer.getContext('2d')
+    canvasContext.clearRect(0,0, width, height)
+    dataArray.forEach((item, index)=>{
+        const y = item /255 * height/2
+        const x = barWidth * index;
+
+        canvasContext.fillStyle= `hsl(${y/height*400}, 100%, 50%)`
+        canvasContext.fillRect(x, height - y, barWidth, y)
+    })
+}
+
+function resize(){
+    visualizer.width = visualizer.clientWidth*window.devicePixelRatio
+    visualizer.height =visualizer.clientHeight*window.devicePixelRatio
 }
