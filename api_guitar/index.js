89886ca9c94d5c2dfd7f0e5dd4e03a833f7b493a
jackokramer <jackokramer@gmail.com>
2020-09-16 11:37:52 -0700

    guitar amp simulator beta

---
+++ api_guitar/index.js
@@ -0,0 +1,29 @@
+const volume = document.getElementById('volume')
+const bass = document.getElementById('bass')
+const mid = document.getElementById('mid')
+const treble = document.getElementById('treble')
+const visualizer = document.getElementById('visualizer')
+
+const context = new AudioContext()
+
+setUpContext()
+
+async function setUpContext (){
+    const guitar = await getGuitar()
+    if (context.state === 'suspended'){
+        await context.resume()
+    }
+    const source = context.createMediaStreamSource(guitar)
+    source.connect(context.destination)
+}
+
+function getGuitar(){
+    return navigator.mediaDevices.getUserMedia({
+        audio: {
+            echoCancellation: false,
+            autoGainControl: false,
+            noiseSuppression: false,
+            latency: 0
+        }
+    })
+}
